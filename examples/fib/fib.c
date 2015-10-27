080db4698702804317790a1558412f73494c07db
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2015-10-26 19:41:30 -0500

    Add examples that demonstrate deadlock problems

---
+++ examples/fib/fib.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <hclib.h>
+
+#define FINISH GEN_FINISH_SCOPE(MACRO_CONCAT(_hcGenVar_, __COUNTER__))
+#define GEN_FINISH_SCOPE(V) for (int V=(start_finish(), 1); V; end_finish(), --V)
+#define MACRO_CONCAT(a, b) DO_MACRO_CONCAT(a, b)
+#define DO_MACRO_CONCAT(a, b) a ## b
+
+typedef struct {
+    int n;
+    long res;
+} FibArgs;
+
+void fib(void * raw_args) {
+    FibArgs *args = raw_args;
+    if (args->n < 2) {
+        args->res = args->n;
+    }
+    else {
+        FibArgs lhsArgs = { args->n - 1, 0 };
+        FibArgs rhsArgs = { args->n - 2, 0 };
+        FINISH {
+            async(fib, &lhsArgs, NO_DDF, NO_PHASER, NO_PROP);
+            async(fib, &rhsArgs, NO_DDF, NO_PHASER, NO_PROP);
+        }
+        args->res = lhsArgs.res + rhsArgs.res;
+    }
+}
+
+int main(int argc, char ** argv) {
+    hclib_init(&argc, argv);
+    const int n = atoi(argv[1]);
+    FibArgs args = { n, 0 };
+    FINISH {
+        async(fib, &args, NO_DDF, NO_PHASER, NO_PROP);
+    }
+    printf("Fib(%d) = %ld\n", n, args.res);
+    hclib_finalize();
+}
